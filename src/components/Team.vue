e56d9304c09698f22db30aeec0e91c4cf68c881b
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-04-27 11:12:58 +0200

    Usable "Our team" segment

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -1,15 +1,20 @@
 <template>
   <section>
     <h2>Our team</h2>
-    <div v-bind:key="person.email" v-for="person in people">
-      <p>{{ person.name }}</p>
+    <div>
+      <TeamMember v-bind:key="person.email" v-for="person in people" :name="person.name" :role="person.role"
+                  :description="person.description" :email="person.email"
+                  :image-path="person.imagePath"/>
     </div>
   </section>
 </template>
 
 <script>
+import TeamMember from "@/components/TeamMember";
+
 export default {
   name: "Team",
+  components: {TeamMember},
   data() {
     return {
       people: [
@@ -17,15 +22,60 @@ export default {
           name: "Per Lindberg",
           role: "CEO",
           email: "per@sunlitsea.no",
-          imagePath: "",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
           description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
-        }
+        },
+        {
+          name: "Per Lindberg",
+          role: "CEO",
+          email: "per2@sunlitsea.no",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
+          description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
+        },
+        {
+          name: "Per Lindberg",
+          role: "CEO",
+          email: "per3@sunlitsea.no",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
+          description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
+        },
+        {
+          name: "Per Lindberg",
+          role: "CEO",
+          email: "per4@sunlitsea.no",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
+          description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
+        },
+        {
+          name: "Per Lindberg",
+          role: "CEO",
+          email: "per5@sunlitsea.no",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
+          description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
+        },
+        {
+          name: "Per Lindberg",
+          role: "CEO",
+          email: "per6@sunlitsea.no",
+          imagePath: require("@/assets/images/IMG_1045.jpeg"),
+          description: "Per lorem ipsum dolor sit amet consectur adsciptig et. With an exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean based floating solar."
+        },
       ]
     };
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+section {
+  @apply px-8 py-12 md:h-screen lg:px-24 lg:pt-0 flex flex-col h-screen;
 
+  h2 {
+    @apply text-4xl font-heading md:text-5xl;
+  }
+
+  & > div {
+    @apply flex my-auto h-1/2 overflow-x-auto;
+  }
+}
 </style>
---
+++ src/components/TeamMember.vue
@@ -0,0 +1,74 @@
+<template>
+  <article>
+    <div>
+      <div>
+        <img :src="imagePath" :alt="'Portrait of ' + name"/>
+      </div>
+
+      <div>
+        <span>{{ name }}</span>
+        <small>{{ role }}</small>
+        <a :href="'mailto:' + email">{{ email }}</a>
+      </div>
+    </div>
+
+    <div>
+      <p>{{ description }}</p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "TeamMember",
+  // NB: image-path is converted to imagePath to make it a valid JS variable
+  props: ['name', 'role', 'email', 'image-path', 'description']
+}
+</script>
+
+<style scoped lang="scss">
+article {
+  @apply my-auto flex flex-col flex-shrink-0 flex-grow-0 mr-8;
+  flex-basis: 25%;
+
+  // Image + Name/role
+  div:first-child {
+    @apply flex flex-grow-0;
+
+    // Image container
+    div:first-child {
+      @apply flex-auto h-72 pr-4 pb-4;
+
+      img {
+        @apply object-cover h-full w-full;
+
+        // Only supported in Chrome as of now, but adding for those who use that.
+        aspect-ratio: 1/1;
+      }
+    }
+
+    // Name/role
+    div:nth-child(2) {
+      span {
+        @apply font-normal;
+      }
+
+      small, a {
+        @apply text-gray-600;
+      }
+
+      span, small, a {
+        @apply block;
+      }
+    }
+
+    // Paragraph
+    div:nth-child(3) {
+    }
+  }
+
+  p {
+    @apply block flex-grow-0;
+  }
+}
+</style>
