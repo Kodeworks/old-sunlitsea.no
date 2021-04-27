634e510b48d68d617ed714fd02417e1c054b740b
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-04-27 16:14:36 +0200

    Update partners segment

--- src/components/Partners.vue
+++ src/components/Partners.vue
@@ -1,8 +1,8 @@
 <template>
   <section>
-    <h2>Partners</h2>
-    <div v-for="img in images" v-bind:key="img.src">
-      <img :src="img.src" :alt="img.alt">
+    <h2>Our Partners</h2>
+    <div>
+      <img v-for="img in images" v-bind:key="img.src" :src="img.src" :alt="img.alt">
     </div>
   </section>
 </template>
@@ -20,7 +20,47 @@ export default {
         {
           src: require("@/assets/partners/ife.png"),
           alt: "IFE"
-        }
+        },
+        {
+          src: require("@/assets/partners/oslomet.png"),
+          alt: "Oslo Met"
+        },
+        {
+          src: require("@/assets/partners/stadtTowingTank.png"),
+          alt: "Stadt Towing Tank"
+        },
+        {
+          src: require("@/assets/partners/accura.png"),
+          alt: "Accura"
+        },
+        {
+          src: require("@/assets/partners/asherGroup.png"),
+          alt: "Asher Group"
+        },
+        {
+          src: require("@/assets/partners/ceiia.png"),
+          alt: "Ceiia"
+        },
+        {
+          src: require("@/assets/partners/innovationNorway.png"),
+          alt: "Innovation Norway"
+        },
+        {
+          src: require("@/assets/partners/kodeworks.png"),
+          alt: "Kodeworks"
+        },
+        {
+          src: require("@/assets/partners/multiconsult.png"),
+          alt: "Multiconsult"
+        },
+        {
+          src: require("@/assets/partners/uio.png"),
+          alt: "UiO"
+        },
+        {
+          src: require("@/assets/partners/solstrom.jpg"),
+          alt: "Solstrom"
+        },
       ]
     }
   }
@@ -29,8 +69,18 @@ export default {
 
 <style scoped lang="scss">
 section {
-  img {
-    @apply h-4;
+  @apply px-8 py-12 lg:px-24 mb-32;
+
+  h2 {
+    @apply font-heading text-5xl mb-12 text-center;
+  }
+
+  div {
+    @apply flex justify-around flex-wrap px-32;
+
+    img {
+      @apply h-10 my-6 mx-2;
+    }
   }
 }
 </style>
--- src/components/Roadmap.vue
+++ src/components/Roadmap.vue
@@ -14,7 +14,7 @@ export default {
 
 <style scoped lang="scss">
   section {
-    @apply h-screen overflow-x-scroll py-72 pl-96 bg-black;
+    @apply h-screen overflow-x-scroll py-72 pl-48 lg:pl-96 bg-black;
 
     svg {
       @apply max-w-none h-full;
