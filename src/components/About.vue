99738687b05b43bf207f0ca3a678516eb1709ae6
vegasten <vegard@kodeworks.no>
2021-06-24 23:54:44 +0200

    Format about section for desktop/mobile

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,23 +1,43 @@
 <template>
   <section>
     <h2>About us</h2>
-    <p>SunlitSea was founded in March 2019 by Per Lindberg and Kodeworks AS as a technology provider to the floating
-      solar industry. With a new and rapidly growing market, SunlitSea aims to address the potential of optimizing cost,
-      longevity, power production, operation and maintenance, degradation, safety and decommissioning. With an
-      exceptional great team and support from our partners we are fit for this job and ready to revolutionize ocean
-      based floating solar.</p>
+    <p>
+      SunlitSea was founded in March 2019 by Per Lindberg and Kodeworks AS as a
+      technology provider to the floating solar industry. With a new and rapidly
+      growing market, SunlitSea aims to address the potential of optimizing
+      cost, longevity, power production, operation and maintenance, degradation,
+      safety and decommissioning. With an exceptional great team and support
+      from our partners we are fit for this job and ready to revolutionize ocean
+      based floating solar.
+    </p>
   </section>
 </template>
 
 <script>
 export default {
-  name: "About"
-}
+  name: 'About',
+};
 </script>
 
 <style scoped lang="scss">
 section {
-  @apply h-screen lg:h-auto flex-col flex justify-center w-1/3 mx-auto lg:my-64;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 8rem;
+  margin-bottom: 8rem;
+
+  p {
+    width: 33%;
+    @media only screen and (max-width: 1200px) {
+      width: 50%;
+    }
+    @media only screen and (max-width: 600px) {
+      width: 90%;
+    }
+  }
 
   h2 {
     @apply text-4xl font-heading md:text-5xl mb-4;
