d54e1fb523d5a3e3e42b453e807675be47b68ee6
vegasten <vegard@kodeworks.no>
2021-06-25 02:26:26 +0200

    Format preview installation for desktop/mobile

--- src/components/PreviewInstallation.vue
+++ src/components/PreviewInstallation.vue
@@ -1,16 +1,26 @@
 <template>
   <section>
     <h2>Floating Solar Solutions</h2>
-    <p>Our goal is to achieve the lowest cost for floating solar electricity which is achieved by combining
-      prefabrication and a very clever design</p>
+    <p>
+      Our goal is to achieve the lowest cost for floating solar electricity
+      which is achieved by combining prefabrication and a very clever design
+    </p>
     <div>
       <div>
-        <img src="../assets/images/previewImage1.png" alt="A folded stack of floaters on the water."/>
+        <img
+          id="stackedFloatersImage"
+          src="../assets/images/previewImage1.png"
+          alt="A folded stack of floaters on the water."
+        />
       </div>
       <div>
-        <img src="../assets/images/previewImage2.png" alt="A grid of floaters on the water."/>
-        <a href="https://iiot.kodeworks.no">Preview a virtual installation
-          <ArrowLeft/>
+        <img
+          src="../assets/images/previewImage2.png"
+          alt="A grid of floaters on the water."
+        />
+        <a href="https://iiot.kodeworks.no"
+          >Preview a virtual installation
+          <ArrowLeft />
         </a>
       </div>
     </div>
@@ -18,24 +28,33 @@
 </template>
 
 <script>
-import ArrowLeft from "@/components/ArrowLeft";
+import ArrowLeft from '@/components/ArrowLeft';
 
 export default {
-  name: "PreviewInstallation",
-  components: {ArrowLeft}
-}
+  name: 'PreviewInstallation',
+  components: { ArrowLeft },
+};
 </script>
 
 <style scoped lang="scss">
 section {
-  @apply px-8 py-12 space-y-8 md:h-screen lg:px-24 lg:pt-0 lg:pb-0 lg:grid-cols-2 lg:space-y-0 flex flex-col justify-center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding-bottom: 6rem;
+  padding-top: 6rem;
+  padding-left: 4rem;
+  padding-right: 4rem;
 
   h2 {
     @apply text-5xl text-center;
-    color: #4B5494;
+    color: #4b5494;
   }
 
-  h2, p {
+  h2,
+  p {
     @apply w-2/3 mx-auto text-center pt-4;
   }
 
@@ -47,11 +66,15 @@ section {
     @apply flex mt-auto mx-auto;
 
     & div:first-child {
-      @apply pr-6 pb-24;
+      @apply pb-24;
+      padding-right: 1.5rem;
+      @media only screen and (max-width: 800px) {
+        padding-right: 0;
+      }
     }
 
     & div:last-child {
-      @apply self-end pl-6;
+      @apply self-end;
 
       a {
         @apply block mt-2;
@@ -61,6 +84,12 @@ section {
         }
       }
     }
+  }
+}
+
+#stackedFloatersImage {
+  @media only screen and (max-width: 800px) {
+    display: none;
   }
 }
 </style>
