7dd91557cd43195a19f8cd080767f32eb31a231a
vegasten <vegard@kodeworks.no>
2021-06-25 12:49:01 +0200

    Add different labels for mobile

--- src/visualisation/script.ts
+++ src/visualisation/script.ts
@@ -1,4 +1,4 @@
-import { MathUtils, Object3D, Mesh, } from "three";
+import { MathUtils, Object3D, Mesh, PlaneGeometry, MeshStandardMaterial, DoubleSide } from "three";
 
 // Something strange with this library, but this works and is how it's supposed to be used according to the documentation.
 // @ts-ignore
@@ -17,8 +17,10 @@ const lerp = MathUtils.lerp;
 export default async (containerSelector: string) => {
     const [scene, camera, renderer] = setup();
 
+    let onMobile;
     if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
-        camera.fov = 120; // Hack
+        camera.fov = 110; // Hack
+        onMobile = true;
     }
 
     const [topGLTF, solarPanelGLTF, bottomGLTF] = await loadModels('Top.glb', 'SolarPanel_smaller.glb', 'Bottom.glb');
@@ -37,15 +39,33 @@ export default async (containerSelector: string) => {
     /**
      * Text labels
      */
-    const topLabel = await svgToGroup("pil1hvit.svg");
-    const middleLabel = await svgToGroup("pil2hvit.svg");
-    const bottomLabel = await svgToGroup("pil3hvit.svg");
-    const hingeLabel = await svgToGroup("top.svg");
-    const cableLabel = await svgToGroup("middle.svg");
-    const fillerLabel = await svgToGroup("bottom.svg");
+
+    let topLabel;
+    let middleLabel;
+    let bottomLabel;
+    let junctionBoxLabel;
+    let sealantLabel;
+    let coolingLabel;
+
+
+    if (onMobile) {
+        topLabel = await svgToGroup("pil1hvit_mobile.svg");
+        middleLabel = await svgToGroup("pil2hvit_mobile.svg");
+        bottomLabel = await svgToGroup("pil3hvit_mobile.svg");
+        junctionBoxLabel = await svgToGroup("pil4hvit_mobile.svg");
+        sealantLabel = await svgToGroup("pil5hvit_mobile.svg");
+        coolingLabel = await svgToGroup("pil6hvit_mobile.svg");
+    } else {
+        topLabel = await svgToGroup("pil1hvit.svg");
+        middleLabel = await svgToGroup("pil2hvit.svg");
+        bottomLabel = await svgToGroup("pil3hvit.svg");
+        junctionBoxLabel = await svgToGroup("pil4hvit.svg");
+        sealantLabel = await svgToGroup("pil5hvit.svg");
+        coolingLabel = await svgToGroup("pil6hvit.svg");
+    }
 
     // Flipping and scaling labels
-    [topLabel, middleLabel, bottomLabel, hingeLabel, cableLabel, fillerLabel].forEach(x => {
+    [topLabel, middleLabel, bottomLabel, junctionBoxLabel, sealantLabel, coolingLabel].forEach(x => {
             x.scale.multiplyScalar(0.01);
             x.scale.y *= -1;
         });
@@ -57,42 +77,93 @@ export default async (containerSelector: string) => {
     const cableObj = new Object3D();
     const fillerObj = new Object3D();
 
-    topLabel.position.x += 0.85;
-    topLabel.position.y += 1.25;
-    topLabel.position.z -= 0;
-    topLabel.rotateY(Math.PI / 4);
+    if (onMobile) {
+        topLabel.scale.multiplyScalar(0.8);
+        topLabel.position.x -= 1.3;
+        topLabel.position.y += 2.9;
+        topLabel.position.z += 0;
+        topLabel.rotateY(Math.PI / 4);
+        topLabel.rotateX(-Math.PI / 15);
 
-    middleLabel.position.x -= 2.7;
-    middleLabel.position.y += 1.1;
-    middleLabel.position.z += 3.8;
-    middleLabel.rotateY(Math.PI / 4);
+        middleLabel.scale.multiplyScalar(0.8);
+        middleLabel.position.x -= 1.7;
+        middleLabel.position.y += 1.9;
+        middleLabel.position.z += 0.8;
+        middleLabel.rotateY(Math.PI / 4);
+        middleLabel.rotateX(-Math.PI / 15);
 
-    bottomLabel.position.x += 1.20;
-    bottomLabel.position.y += 1.20;
-    bottomLabel.position.z += 0.30;
-    bottomLabel.rotateY(Math.PI / 4);
+        bottomLabel.scale.multiplyScalar(0.6);
+        bottomLabel.position.x += 0.2;
+        bottomLabel.position.y += 0;
+        bottomLabel.position.z += 1.2;
+        bottomLabel.rotateY(Math.PI / 4);
+        bottomLabel.rotateX(-Math.PI / 15);
 
-    hingeLabel.scale.multiplyScalar(0.75);
-    hingeLabel.position.y += 0.73;
-    hingeLabel.position.z -= 1.05;
-    hingeLabel.position.x -= 0.5;
-    hingeLabel.rotateY(Math.PI / 2);
+        junctionBoxLabel.scale.multiplyScalar(0.75);
+        junctionBoxLabel.position.x += 0.90;
+        junctionBoxLabel.position.y += 2.05;
+        junctionBoxLabel.position.z += 0.55;
+        junctionBoxLabel.rotateY(Math.PI / 2);
+        junctionBoxLabel.rotateX(-Math.PI / 15);
 
-    cableLabel.scale.multiplyScalar(0.6);
-    cableLabel.position.y += 0.77;
-    cableLabel.position.z += 0.5;
-    cableLabel.position.x += 2;
-    cableLabel.rotateY(Math.PI / 2);
+        sealantLabel.scale.multiplyScalar(0.75);
+        sealantLabel.position.y += 3;
+        sealantLabel.position.z += 0.4;
+        sealantLabel.position.x += 0.9;
+        sealantLabel.rotateY(Math.PI / 2);
+        sealantLabel.rotateX(-Math.PI / 15);
 
-    fillerLabel.scale.multiplyScalar(0.9);
-    fillerLabel.position.y += 0.4;
-    fillerLabel.position.x += 1;
-    fillerLabel.position.z += 0.01;
-    fillerLabel.rotateY(Math.PI / 2);
+        coolingLabel.scale.multiplyScalar(0.75);
+        coolingLabel.position.y -= 0.0;
+        coolingLabel.position.x += 1;
+        coolingLabel.position.z += 1.5;
+        coolingLabel.rotateY(Math.PI / 2);
+        coolingLabel.rotateX(-Math.PI / 15)
+    } else {
+        topLabel.position.x += 0.85;
+        topLabel.position.y += 1.25;
+        topLabel.position.z -= 0;
+        topLabel.rotateY(Math.PI / 4);
+        topLabel.rotateX(-Math.PI / 15);
+
+        middleLabel.position.x -= 2.7;
+        middleLabel.position.y += 1.1;
+        middleLabel.position.z += 3.8;
+        middleLabel.rotateY(Math.PI / 4);
+        middleLabel.rotateX(-Math.PI / 15);
+
+        bottomLabel.position.x += 1.20;
+        bottomLabel.position.y += 1.20;
+        bottomLabel.position.z += 0.30;
+        bottomLabel.rotateY(Math.PI / 4);
+        bottomLabel.rotateX(-Math.PI / 15);
+
+        junctionBoxLabel.scale.multiplyScalar(0.75);
+        junctionBoxLabel.position.x += 0.90;
+        junctionBoxLabel.position.y += 0.8;
+        junctionBoxLabel.position.z += 2.9;
+        junctionBoxLabel.rotateY(Math.PI / 2);
+        junctionBoxLabel.rotateX(-Math.PI / 15);
+
+        sealantLabel.scale.multiplyScalar(0.75);
+        sealantLabel.position.y += 0.8;
+        sealantLabel.position.z -= 2.0;
+        sealantLabel.position.x += 0.9;
+        sealantLabel.rotateY(Math.PI / 2);
+        sealantLabel.rotateX(-Math.PI / 15);
+
+
+        coolingLabel.scale.multiplyScalar(0.75);
+        coolingLabel.position.y -= 0.0;
+        coolingLabel.position.x += 1;
+        coolingLabel.position.z += 1.5;
+        coolingLabel.rotateY(Math.PI / 2);
+        coolingLabel.rotateX(-Math.PI / 15);
+    }
 
     topObj.add(topLabel, solarPanelGLTF.scene);
     middleObj.add(middleLabel, topGLTF.scene);
-    bottomObj.add(bottomLabel, bottomGLTF.scene);    
+    bottomObj.add(bottomLabel, bottomGLTF.scene);
 
     topObj.rotateY(-Math.PI / 4);
     middleObj.rotateY(-Math.PI / 4);
@@ -101,9 +172,9 @@ export default async (containerSelector: string) => {
     neighbourObj.position.x = 10;
     neighbourObj.rotation.y = -Math.PI * 0.5; // Edge with notch pointing at camera
 
-    neighbourObj.add(hingeLabel, neighbourGLTF.scene);
+    neighbourObj.add(junctionBoxLabel, neighbourGLTF.scene);
 
-    cableObj.add(cableLabel, cableGLTF.scene);
+    cableObj.add(sealantLabel, cableGLTF.scene);
     cableObj.rotateY(-Math.PI / 2);
     cableObj.position.x -= 0.01;
 
@@ -138,7 +209,7 @@ export default async (containerSelector: string) => {
     });
 
     const secondFillerObj = fillerObj.clone();
-    secondFillerObj.add(fillerLabel);
+    secondFillerObj.add(coolingLabel);
     secondFillerObj.position.x += 2.05;
     /**
      * ADD EM ALL TO THE SCENE
@@ -153,18 +224,27 @@ export default async (containerSelector: string) => {
     scene.add(fillerObj);
     scene.add(secondFillerObj);
 
+    // const planeGeometry = new PlaneGeometry(100 , 100);
+    // const planeMaterial = new MeshStandardMaterial({ color: 0x111111, side: DoubleSide });
+    // const plane = new Mesh(planeGeometry, planeMaterial);
+    // plane.rotateX(- Math.PI / 2);
+    // plane.position.y -= 2;
+
+    // scene.add(plane);
+
+
     /**
      * Event handlers
      */
 
     const labelMaterials = extractMaterials(topLabel, middleLabel, bottomLabel);
-    const secondLabelMaterials = extractMaterials(hingeLabel, cableLabel, fillerLabel);
+    const secondLabelMaterials = extractMaterials(junctionBoxLabel, sealantLabel, coolingLabel);
 
     const parentContainer: HTMLElement = document.querySelector(containerSelector)! as HTMLElement;
 
     let scrollPercentage = 0;
 
-    const onScroll = () => { 
+    const onScroll = () => {
         const rect = parentContainer.getBoundingClientRect();
         const inView = rect.top <= 0  && rect.bottom >= 0;
 
@@ -212,15 +292,28 @@ export default async (containerSelector: string) => {
         duration: 1000,
     }, 500);
 
-    // Move camera out
-    tl.add({
-        targets: camera.position,
-        x: 0,
-        y: 1.5,
-        z: 6,
-        duration: 1000,
-        update: () => camera.updateProjectionMatrix()
-    }, 500);
+
+    if (onMobile) {
+        // Move camera out
+        tl.add({
+            targets: camera.position,
+            x: 0,
+            y: 1,
+            z: 3,
+            duration: 1000,
+            update: () => camera.updateProjectionMatrix()
+        }, 500);
+    } else {
+        // Move camera out
+        tl.add({
+            targets: camera.position,
+            x: 0,
+            y: 1.5,
+            z: 6,
+            duration: 1000,
+            update: () => camera.updateProjectionMatrix()
+        }, 500);
+    }
 
     // Fade in labels for top/mid/bot
     tl.add({
@@ -258,7 +351,7 @@ export default async (containerSelector: string) => {
         z: 4,
         duration: 400,
         update: () => camera.updateProjectionMatrix()
-    }, '-=250');    
+    }, '-=250');
 
     // Slide the camera to the connectors
     tl.add({
@@ -287,11 +380,11 @@ export default async (containerSelector: string) => {
     }, "-=1000");
 
     // TODO Find a better way to select material
-    // Fade in cable 
+    // Fade in cable
     tl.add({
         targets: [...cableMaterials],
         opacity: 1,
-        duration: 1000,        
+        duration: 1000,
     }, '-=0');
 
     // Fade in filler
@@ -315,7 +408,7 @@ export default async (containerSelector: string) => {
         opacity: 0,
         duration: 250,
     }, '+=500');
-   
+
     //Fade out cable
     tl.add({
         targets: [...cableMaterials],
@@ -354,23 +447,17 @@ export default async (containerSelector: string) => {
         duration: 1000,
         easing: 'easeInOutExpo',
         update: () => camera.updateProjectionMatrix()
-    }, '-=500');  
-    
+    }, '-=500');
+
     /**
      * PERFORM THE RENDERING
      */
-    
+
     let animationPercentage = 0;
 
 
     const render = () => {
         requestAnimationFrame(render);
-
-        // spotlight.position.set(
-        //     camera.position.x + 5,
-        //     camera.position.y + 5,
-        //     camera.position.z + 5,
-        // );
 
         animationPercentage = lerp(animationPercentage, scrollPercentage, 0.1); // Smooth out the mapping from scrolling
         tl.seek(animationPercentage * TIMELINE_DURATION);
